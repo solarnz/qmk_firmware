1773d825ce9d70c12f8fc90e9df579eef6281ae4
Chris Trotman <chris@trotman.io>
2022-01-17 18:58:39 +1100

    teck/home: Make left tab send control when held

--- keyboards/solarnz_teck/keymaps/home/keymap.c
+++ keyboards/solarnz_teck/keymaps/home/keymap.c
@@ -10,7 +10,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     /* 0: qwerty */
     [_MA] = LAYOUT(
         KC_RALT,   KC_RIGHT,  KC_DOWN,   KC_UP,     KC_LEFT,   KC_BSPC,   KC_END,    KC_SPC,    KC_LGUI,   KC_PGDN,   KC_HOME,   KC_PGUP,   KC_LCTRL, \
-        KC_ENT,    KC_QUOT,   KC_DOT,    KC_COMM,   KC_M,      KC_B,      KC_V,      KC_N,      KC_LCTRL,  KC_C,      KC_Z,      KC_X,      KC_TAB, \
+        KC_ENT,    KC_QUOT,   KC_DOT,    KC_COMM,   KC_M,      KC_B,      KC_V,      KC_N,      KC_LCTRL,  KC_C,      KC_Z,      KC_X,      MT(MOD_LCTL, KC_TAB), \
         KC_LBRC,   KC_P,      KC_O,      KC_I,      KC_U,      KC_T,      KC_R,      KC_Y,      KC_DEL,    KC_E,      KC_Q,      KC_W,      KC_SLASH, \
         KC_RSFT,   KC_SCLN,   KC_L,      KC_K,      KC_J,      KC_G,      KC_F,      KC_H,      KC_U,      KC_D,      KC_A,      KC_S,      KC_LSFT, \
         KC_EQL,    KC_RBRC,   KC_S,      KC_D,      KC_F,      KC_CALC,   KC_TAB,    KC_PSCR,   KC_FN0,    KC_K,      KC_BSLS,   KC_ESC,    KC_ESC, \
