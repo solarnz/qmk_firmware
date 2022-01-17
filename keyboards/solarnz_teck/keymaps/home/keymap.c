825e70cfe17c4b1869c97348aca860e9a1ed790f
Chris Trotman <chris@trotman.io>
2022-01-17 18:58:56 +1100

    teck: Fix FN key

--- keyboards/solarnz_teck/keymaps/home/keymap.c
+++ keyboards/solarnz_teck/keymaps/home/keymap.c
@@ -13,7 +13,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_ENT,    KC_QUOT,   KC_DOT,    KC_COMM,   KC_M,      KC_B,      KC_V,      KC_N,      KC_LCTRL,  KC_C,      KC_Z,      KC_X,      MT(MOD_LCTL, KC_TAB), \
         KC_LBRC,   KC_P,      KC_O,      KC_I,      KC_U,      KC_T,      KC_R,      KC_Y,      KC_DEL,    KC_E,      KC_Q,      KC_W,      KC_SLASH, \
         KC_RSFT,   KC_SCLN,   KC_L,      KC_K,      KC_J,      KC_G,      KC_F,      KC_H,      KC_U,      KC_D,      KC_A,      KC_S,      KC_LSFT, \
-        KC_EQL,    KC_RBRC,   KC_S,      KC_D,      KC_F,      KC_CALC,   KC_TAB,    KC_PSCR,   KC_FN0,    KC_K,      KC_BSLS,   KC_ESC,    KC_ESC, \
+        KC_EQL,    KC_RBRC,   KC_S,      KC_D,      KC_F,      KC_CALC,   KC_TAB,    KC_PSCR,   MO(_FN),   KC_K,      KC_BSLS,   KC_ESC,    KC_ESC, \
         KC_TAB,    KC_VOLU,   KC_VOLD,   KC_MUTE,   KC_MNXT,   KC_F6,     KC_F5,     KC_MPLY,   KC_MPRV,   KC_LGUI,   KC_DOWN,   KC_UP,     KC_RIGHT, \
         KC_MINS,   KC_0,      KC_9,      KC_8,      KC_7,      KC_5,      KC_4,      KC_6,      KC_LGUI,   KC_3,      KC_1,      KC_2,      KC_GRV  \
     ),
--- keyboards/solarnz_teck/keymaps/work/keymap.c
+++ keyboards/solarnz_teck/keymaps/work/keymap.c
@@ -10,7 +10,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     /* 0: qwerty */
     [_MA] = LAYOUT(
         KC_ESC,    KC_F1,     KC_F2,     KC_F3,     KC_F4,     KC_F5,     KC_F6,     KC_MPRV,   KC_MPLY,   KC_MNXT,   KC_MUTE,   KC_VOLD,   KC_VOLU, \
-        KC_NO,     KC_ESC,    KC_BSLS,   KC_NO,     KC_NO,     KC_NO,     KC_FN0,    KC_NLCK,   KC_NO,     KC_NO,     KC_NO,     KC_EQL,    KC_RBRC, \
+        KC_NO,     KC_ESC,    KC_BSLS,   KC_NO,     KC_NO,     KC_NO,     MO(_FN),   KC_NLCK,   KC_NO,     KC_NO,     KC_NO,     KC_EQL,    KC_RBRC, \
         KC_GRV,    KC_1,      KC_2,      KC_3,      KC_4,      KC_5,      KC_LGUI,   KC_6,      KC_7,      KC_8,      KC_9,      KC_0,      KC_MINS, \
         KC_SLASH,  KC_Q,      KC_W,      KC_E,      KC_R,      KC_T,      KC_DEL,    KC_Y,      KC_U,      KC_I,      KC_O,      KC_P,      KC_LBRC, \
         KC_LSFT,   KC_A,      KC_S,      KC_D,      KC_F,      KC_G,      KC_TAB,    KC_H,      KC_J,      KC_K,      KC_L,      KC_SCLN,   KC_RSFT, \
